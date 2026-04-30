1725ce5e2feae28cf9001ef2da747e9a2058c0e1
learning909-tech <learningtech909@gmail.com>
2026-05-01 00:37:12 +0530

    Fix formatting in start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull learningtech909/python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 learningtech909/python-flask-app
+docker run -d -p 5000:5000 learningtech909/python-flask-app 
